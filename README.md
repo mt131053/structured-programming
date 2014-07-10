408c6e4c3619f085eaef4c1f6e4a292b7347a9d2
mt13053 <m.topuzov88@gmail.com>
2014-07-10 14:17:20 +0200

    Upload exercises

---
+++ README.md
@@ -0,0 +1,12 @@
+Structured Programming
+======================
+
+Classroom Exercises
+
+Laboratory Exercises
+ 
+Book 1 - "Збирка решени задачи Програмски јазик C"
+
+Book 2 - "Збирка алгоритми и програми"
+
+All mathematical formulas generated with the [Unicode Pretty Printer](http://docs.sympy.org/dev/tutorial/printing.html) from the Python library [SymPy](http://sympy.org/en/index.html)
---
+++ laboratory_exercises/lab-exer-9/exer-42.c
@@ -0,0 +1,20 @@
+/******************************************************************************/
+/* Да се напише програма која од датотеката со име "text.txt" ќе ги одреди и  */
+/* отпечати на стандарден излез односот на цифри и букви.                     */
+/*                                                                            */
+/* Пример влез:                                                               */
+/* It is preferable to avoid 4134sav54543253ing empty li432432434nes on the h */
+/* istory list, since users rarely have a burning need to reuse a blan7777k l */
+/* ine. Here is a 23 function which 3 usefully repla98343242ces the 42134 435 */
+/* 4678789876978standard gets() library function, and has the advantage of no */
+/* static buffer to overflow                                                  */
+/*                                                                            */
+/* Пример излез:                                                              */
+/* 0.27                                                                       */
+/******************************************************************************/
+
+int main()
+{
+
+    return 0;
+}
